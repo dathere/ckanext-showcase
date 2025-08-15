8a0f8f2460855f175c8d7a57eba9d38665b5c196
Mohammed Minhajuddin <minhajuddin2510@gmail.com>
2025-08-15 07:12:26 -0400

    Update plugin.py

--- ckanext/showcase/plugin.py
+++ ckanext/showcase/plugin.py
@@ -94,6 +94,8 @@ class ShowcasePlugin(plugins.SingletonPlugin, lib_plugins.DefaultDatasetForm):
             'facet_remove_field': showcase_helpers.facet_remove_field,
             'get_site_statistics': showcase_helpers.get_site_statistics,
             'showcase_get_wysiwyg_editor': showcase_helpers.showcase_get_wysiwyg_editor,
+            'get_value_from_showcase_extras': showcase_helpers.get_value_from_showcase_extras,
+            'get_thumbnail': showcase_helpers.get_thumbnail,
         }
 
     # IFacets
